243556257acd0467c52b96dfedde1c02681b41c2
Julian Betz <33902330+julianbetz@users.noreply.github.com>
2021-02-17 19:15:41 +0100

    Add Emacs local variables
    The files `.dir-locals.el' and `.dir-locals-2.el' are meant
    for public (i.e. version-controlled) and private (i.e. not
    version-controlled) specifications of directory-local
    variables, respectively.

---
+++ .dir-locals-2.el
@@ -0,0 +1,6 @@
+;;; Private directory local variables
+
+;; This file should NOT be under version control.
+;; For more information see (info "(emacs) Directory Variables").
+
+()
---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Public directory local variables
+
+;; This file should be under version control.
+;; For more information see (info "(emacs) Directory Variables").
+
+()
